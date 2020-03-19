e2c89c8529f3852d226c705884cfe8ecd96339e6
Michael Boyce <raspberrypicardbox@gmail.com>
2020-03-19 14:15:35 +0000

    Initial commit

---
+++ GitBashImplementation/GitBashImplementation/InputSanitisation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GitBashImplementation
+{
+    class InputSanitisation
+    {
+        public List<string> Input()
+        {
+            string userCommand = Console.ReadLine().ToLower();
+
+            if (userCommand.Contains("diff"))
+            {
+                string[] commandSplit = userCommand.Split(" ");
+                if (!commandSplit[1].Contains(".txt") || !commandSplit[2].Contains(".txt"))
+                {
+                    Console.WriteLine("\nParamaters are invalid!\n");
+                    return Input();
+                }
+                else
+                {
+                    List<string> fileNames = new List<string> { commandSplit[1], commandSplit[2] };
+                    return fileNames;
+                }
+            }
+            else
+            {
+                Console.WriteLine("\nCommand is invalid!\n");
+                return Input();
+            }
+        }
+    }
+}
---
+++ GitBashImplementation/GitBashImplementation/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GitBashImplementation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InputSanitisation userInput = new InputSanitisation();
+
+            Console.WriteLine("-----Welcome to Git Bash C#-----\n");
+
+            List<string> fileNames = userInput.Input();
+
+            string fileOne = System.IO.File.ReadAllText(@$"../../../../Files/{fileNames[0]}");
+            string fileTwo = System.IO.File.ReadAllText(@$"../../../../Files/{fileNames[1]}");
+
+            if (fileOne == fileTwo)
+            {
+                Console.WriteLine($"\n{fileNames[0]} and {fileNames[1]} are not different.");
+            }
+            else
+            {
+                Console.WriteLine($"\n{fileNames[0]} and {fileNames[1]} are different.");
+            }
+        }
+    }
+}
